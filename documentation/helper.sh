4a4aa8613af316e5307c686d776266f6fc9c3ef9
Jacob B. Sanders <jacob.sanders@cloudhybrid.io>
2022-07-11 23:21:16 -0500

    JSON

--- documentation/helper.sh
+++ documentation/helper.sh
@@ -2,4 +2,4 @@
 
 echo "# $(basename "$(git rev-parse --show-toplevel)") #" > "$(pwd)/documentation/header.md"
 
-terraform output --json > "$(pwd)/documentation/header.md"
+terraform output --json > "$(pwd)/documentation/output.json"
